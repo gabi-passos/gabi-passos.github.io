ba75b80249199da29719fdd7bceaa88aa9d9cbda
n-prado <gabiferpassos@gmail.com>
2025-05-24 19:04:56 -0300

    commit inicial

---
+++ FERIAS.js
@@ -0,0 +1,20 @@
+//Executa assim que terminar de carregar a pagina
+document.addEventListener("DOMContentLoaded", function () {
+    //Define o que vai acontecer a cada 1s
+    setInterval(function () {
+        //Inicia uma variavel com a data do inicio das ferias
+        const inicioFerias = new Date(2025, 4, 30, 17, 30)
+        //Inicia uma variavel com a data de hoje
+        const dataAtual = new Date()
+        
+        let diferenca = inicioFerias - dataAtual
+        let dateDiff = new Date()
+        dateDiff.setTime(diferenca)        
+
+        //Busca a diferenca de dias ate a data
+        document.getElementById("dias").textContent = dateDiff.getDate() - 1 + " dias"
+        document.getElementById("horas").textContent = dateDiff.getHours() + "h"
+        document.getElementById("minutos").textContent = dateDiff.getMinutes() + "m"
+        document.getElementById("segundos").textContent = dateDiff.getSeconds()+ "s"
+    }, 1000)
+});
